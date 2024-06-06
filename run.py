7db9d4ec2a3d6bd0dd317d61d077a46de95dd8b6
Bula23 <markowski.ad@gmail.com>
2024-06-06 03:10:05 -0700

    dodanie obslugi bledow

--- run.py
+++ run.py
@@ -2,9 +2,39 @@ from pakiet.bankomat import Bankomat
 
 print("witam w programie")
 
-bankomat1 = Bankomat()
-bankomat1.sprawdz_saldo()
-bankomat1.wplata(1000)
-bankomat1.sprawdz_saldo()
-bankomat1.wyplata(500)
-bankomat1.sprawdz_saldo()
+# bankomat1 = Bankomat()
+# bankomat1.sprawdz_saldo()
+# bankomat1.wplata(1000)
+# bankomat1.sprawdz_saldo()
+# bankomat1.wyplata(500)
+# bankomat1.sprawdz_saldo()
+
+def multi(a, b):
+    try:
+        return a * b
+    except (TypeError, ValueError):
+        return 0
+
+def multi2(a, b):
+    try:
+        return a * b
+    except Exception as e:
+        return(e)
+
+def multi3(a, b):
+    try:
+        suma = a * b
+    except TypeError:
+        return 0
+    except ValueError:
+        return 1
+    else:
+        return a * b
+    finally:
+        print("dzialanie zostalo zakonczone")
+
+
+print(multi(5,5))
+print(multi("5", "tomek"))
+print(multi2("5", "tomek"))
+print(multi3(5, 5))
